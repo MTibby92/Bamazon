d1e9286192ce261c9e7012dc85a0c2b968fa3635
MTibby92 <MTibby92@gmail.com>
2016-10-14 17:21:45 -0500

    added database connection a first sql pull

--- BamazonManager.js
+++ BamazonManager.js
@@ -1,3 +1,15 @@
 var mysql = require('mysql')
 var inquirer = require('inquirer')
 var secret_key = require('./secret_key')
+
+var conn = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: secret_key.password,
+	database: 'bamazon'
+})
+
+conn.query('SELECT itemID, productName, price FROM products', function(err, res) {
+	console.log(res)
+})
