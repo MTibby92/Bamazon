0af45d8e9cd8d8e0c578fe39a7c094b8da565c03
MTibby92 <MTibby92@gmail.com>
2016-10-15 12:05:03 -0500

    got the inquirer prompts working with intial filtering

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -10,6 +10,60 @@ var conn = mysql.createConnection({
 	database: 'bamazon'
 })
 
-conn.query('SELECT itemID, productName, price FROM products', function(err, res) {
-	console.log(res)
+var listofProducts = []
+var selectedProduct = undefined
+
+conn.query('SELECT itemID, productName, price, stockQuantity FROM products', function(err, res) {
+	// console.log(res)
+	console.log('================')
+	console.log('id                name                   price')
+	for (var i in res) {
+		console.log(res[i].itemID + '                 ' + res[i].productName + '              ' + res[i].price)
+		var obj = {
+			itemID : res[i].itemID,
+			productName: res[i].productName,
+			price: res[i].price,
+			stockQuantity: res[i].stockQuantity
+		}
+		listofProducts.push(obj)
+	}
+	console.log(listofProducts)
+
+	inquirer.prompt(prompts).then(function(answers) {
+		console.log(answers)
+	})
 })
+
+var prompts = 
+[{
+	type: 'input',
+	message: 'Please enter the id of the product you would like to purchase',
+	name: 'id',
+	filter: function(id) {
+		return parseInt(id)
+	},
+	validate: function(id) {
+		for (var i in listofProducts) {
+			if (listofProducts[i].itemID == parseInt(id)) {
+				selectedProduct = id
+				return true
+			}
+		}
+		return 'That product id is not available'
+	}
+}, {
+	type: 'input',
+	message: 'How many units of this product would you like to purchase?',
+	name: 'quantity',
+	filter: function(unit) {
+		return parseInt(unit)
+	},
+	validate: function(unit) {
+		for (var i in listofProducts) {
+			if (listofProducts[i].itemID == selectedProduct && listofProducts[i].stockQuantity >= parseInt(unit)) {
+				return true
+			}
+		}
+		return 'That amount is not currently availabe'
+	}
+}]
