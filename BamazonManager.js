fbb3887be07d2cc1968b802d25c60f6c02d40ae4
MTibby92 <MTibby92@gmail.com>
2016-10-15 13:42:44 -0500

    started work on manager file, first command done

--- BamazonManager.js
+++ BamazonManager.js
@@ -10,6 +10,70 @@ var conn = mysql.createConnection({
 	database: 'bamazon'
 })
 
-conn.query('SELECT itemID, productName, price FROM products', function(err, res) {
-	console.log(res)
+var prompts = 
+[
+	{
+		type: 'list',
+		message: 'What would you like to do?',
+		name: 'command',
+		choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory' , 'Add New Product'],
+		filter: function(choice) {
+			switch(choice) {
+				case 'View Products for Sale':
+					return 'viewProd'
+					break
+				case 'View Low Inventory':
+					return 'viewInven'
+					break
+				case 'Add to Inventory':
+					return 'addInven'
+					break
+				case 'Add New Product':
+					return 'addNewProd'
+					break
+			}
+		}
+	}
+]
+
+inquirer.prompt(prompts).then(function(answer) {
+	console.log(answer)
+	switch(answer.command) {
+		case 'viewProd':
+			viewProducts()
+			break
+		case 'viewInven':
+			viewLowInventory()
+			break
+		case 'addInven':
+			addInventory()
+			break
+		case 'addNewProd':
+			addNewProduct()
+			break
+	}
 })
+
+function viewProducts() {
+	conn.query('SELECT itemID, productName, price, stockQuantity FROM products', function(err, res) {
+		if (err) throw err;
+		// console.log(res)
+		console.log('================')
+		console.log('id                name                   price                   quantity in stock')
+		for (var i in res) {
+			console.log(res[i].itemID + '                 ' + res[i].productName + '              $' + res[i].price + '               ' + res[i].stockQuantity)
+		}
+	})
+}
+
+function viewLowInventory() {
+
+}
+
+function addInventory() {
+
+}
+
+function addNewProduct() {
+
+}
