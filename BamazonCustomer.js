c74c0fb4d2f3845f9e96cfec5acae6393b8e19de
MTibby92 <MTibby92@gmail.com>
2016-10-15 13:14:59 -0500

    wrapped code up in a display function and have the order total displaying

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -15,22 +15,13 @@ var selectedProduct = undefined
 
 conn.query('SELECT itemID, productName, price, stockQuantity FROM products', function(err, res) {
 	// console.log(res)
-	console.log('================')
-	console.log('id                name                   price')
-	for (var i in res) {
-		console.log(res[i].itemID + '                 ' + res[i].productName + '              ' + res[i].price)
-		var obj = {
-			itemID : res[i].itemID,
-			productName: res[i].productName,
-			price: res[i].price,
-			stockQuantity: res[i].stockQuantity
-		}
-		listofProducts.push(obj)
-	}
-	console.log(listofProducts)
+	if (err) throw err;
+	
+	displayItems(res)
 
 	inquirer.prompt(prompts).then(function(answers) {
-		console.log(answers)
+		// console.log(answers)
+		placeOrder(answers)
 	})
 })
 
@@ -67,3 +58,37 @@ var prompts =
 		return 'That amount is not currently availabe'
 	}
 }]
+
+
+function displayItems(res) {
+	console.log('================')
+	console.log('id                name                   price')
+	for (var i in res) {
+		console.log(res[i].itemID + '                 ' + res[i].productName + '              $' + res[i].price)
+		var obj = {
+			itemID : res[i].itemID,
+			productName: res[i].productName,
+			price: res[i].price,
+			stockQuantity: res[i].stockQuantity
+		}
+		listofProducts.push(obj)
+	}
+	console.log(listofProducts)
+}
+
+
+
+function placeOrder(input) {
+	conn.query('UPDATE products SET stockQuantity = stockQuantity - ' + conn.escape(input.quantity) + ' WHERE itemID = ' + conn.escape(input.id), function(err,res) {
+		if (err) throw err;
+
+		console.log('Update has completed:', input.id, input.quantity)
+		for (var i in listofProducts) {
+			if (listofProducts[i].itemID == input.id)
+				var price = listofProducts[i].price
+		}
+
+		var total = price * input.quantity
+		console.log('The total price for your order is $' + total)
+	})
+}
