62bdde61fe96d46b5f35619393d6e8beb8d528ab
MTibby92 <MTibby92@gmail.com>
2016-10-17 15:11:03 -0500

    finished update inventory function, working on debugging add product function

--- BamazonManager.js
+++ BamazonManager.js
@@ -12,7 +12,7 @@ var conn = mysql.createConnection({
 
 var listofProducts = []
 conn.query('SELECT itemID, productName, price, stockQuantity FROM products', function(err, res) {
-	if (err) throw err;
+	if (err) {throw err}
 
 	for (var i in res) {
 		var obj = {
@@ -56,7 +56,7 @@ var prompts =
 
 function viewProducts() {
 	conn.query('SELECT itemID, productName, price, stockQuantity FROM products', function(err, res) {
-		if (err) throw err;
+		if (err) {throw err}
 		// console.log(res)
 		console.log('================')
 		console.log('id                name                   price                   quantity in stock')
@@ -68,7 +68,7 @@ function viewProducts() {
 
 function viewLowInventory() {
 	conn.query('SELECT itemID, productName, price, stockQuantity from products WHERE stockQuantity < 5', function(err, res) {
-		if (err) throw err;
+		if (err) {throw err}
 
 		// console.log(res)
 		if (res.length == 0) {
@@ -87,7 +87,7 @@ function viewLowInventory() {
 function addInventory() {
 	var productChoices = []
 	for (var i in listofProducts) {
-		productChoices.push(listofProducts[i].productName)
+		productChoices.push(listofProducts[i].itemID + ' ' + listofProducts[i].productName)
 	}
 
 	var prompt2 = 
@@ -117,9 +117,69 @@ function addInventory() {
 
 	inquirer.prompt(prompt2).then(function(answer) {
 		console.log(answer)
+		// console.log(answer.product.split(' '))
+		var id = answer.product.split(' ')
+		// console.log(id[0])
+		conn.query(`UPDATE products SET stockQuantity = stockQuantity + ${answer.number} WHERE itemID = ${parseInt(id[0])};`, function(err,res) {
+			if (err) {throw err}
+
+			console.log(`Inventory updated! ${answer.product} +${answer.number}`)			
+		})
 	})
 }
 
 function addNewProduct() {
+	var prompt3 = 
+	[
+		{
+			type: 'input',
+			message: 'Enter the name of the product you\'d like to add',
+			name: 'productName'
+		},
+		{
+			type: 'input',
+			message: 'Enter the name of the department the product belongs in',
+			name: 'departmentName'
+		},
+		{
+			type: 'input',
+			message: 'Enter the price of the product',
+			name: 'productPrice',
+			filter: function(num) {
+				return parseFloat(num)
+			},
+			validate: function(num) {
+				if (isNaN(parseFloat(num))) {
+					return 'Please enter a numeric value'
+				} else {
+					return true
+				}
+			}
+		},
+		{
+			type: 'input',
+			message: 'How many items would you like to add to the inventory?',
+			name: 'stockQuantity',
+			filter: function(num) {
+				return parseInt(num)
+			},
+			validate: function(num) {
+				if (isNaN(parseInt(num))) {
+					return 'Please enter a numeric value'
+				} else {
+					return true
+				}
+			}
+		}
+	]
 
+	inquirer.prompt(prompt3).then(function(answer) {
+		console.log(answer)
+
+		conn.query(`INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES (${answer.productName}, ${answer.departmentName}, ${answer.productPrice}, ${answer.stockQuantity});`, function(err,res) {
+			if (err) {throw err}
+
+			console.log(`New Product added ${answer.productName}, ${answer.departmentName}, ${answer.productPrice}, ${answer.stockQuantity}`)
+		})
+	})
 }
