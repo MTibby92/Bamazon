ec5bb6acca1934341e595bb09426d1c106120054
MTibby92 <MTibby92@gmail.com>
2016-10-18 16:01:14 -0500

    preliminary skeleton for executive file

--- BamazonExecutive.js
+++ BamazonExecutive.js
@@ -1,3 +1,42 @@
 var mysql = require('mysql')
 var inquirer = require('inquirer')
-var secret_key = require('./secret_key')
+var secretKey = require('./secret_key')
+
+var conn = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: secretKey.password,
+	database: 'bamazon'
+})
+
+var prompt1 = 
+[
+	{
+		type: 'list',
+		name: 'command',
+		message: 'What would you like to do?',
+		choices: ['View Product Sales by Department', 'Create New Department']
+	},
+]
+
+var departmentList = []
+
+conn.query('SELECT departmentName FROM departments', function(err, res) {
+	if (err) {throw err}
+
+	for (var i in res) {
+		departmentList.push(res[i].departmentName)
+	}
+
+	inquirer.prompt(prompt1).then(function(answer) {
+		switch(answer.command) {
+			case 'View Product Sales by Department':
+				viewSales()
+				break
+			case 'Create New Department':
+				createDepartment()
+				break
+		}
+	})
+})
